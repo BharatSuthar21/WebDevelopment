ed32a5ff1a859df283ea8dc2aa41420f7242540a
shekharshrivas <124181313+shekharshrivas@users.noreply.github.com>
2024-09-16 22:48:33 +0530

    Shekhar (#7)
    * Some changes made in App, Bot, Screen User for batter chat response
    
    * some more changes made in file for good user experience
    
    * Some changes made in css

--- HomeWork1/src/App.jsx
+++ HomeWork1/src/App.jsx
@@ -27,7 +27,6 @@ function App() {
   };
 
   return (
-    <div>
         <div className="chat-container">
           <Screen history={history} />
           <div>
@@ -37,7 +36,6 @@ function App() {
             </div>
           </div>
         </div>
-    </div>
   );
 }
 
